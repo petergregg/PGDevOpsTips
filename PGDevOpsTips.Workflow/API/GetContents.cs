75f7f7b01e4298ca780112a3b81dc0135895b13a
Peter Gregg <greggp1987@hotmail.com>
2023-11-19 12:19:50 +0000

    Add GetContents endpoint to api

---
+++ PGDevOpsTips.Workflow/API/GetContents.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using PGDevOpsTips.Domain.Interfaces;
+using System.Collections.Generic;
+using PGDevOpsTips.Domain.Models;
+
+namespace PGDevOpsTips.Workflow.API
+{
+    public sealed class GetContents
+    {
+        private readonly IStorageService _storageService;
+
+        public GetContents(IStorageService storageService)
+        {
+            _storageService = storageService;
+        }
+
+        [FunctionName("GetContents")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "contents")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string type = req.Query["type"];
+
+            if (type == null)
+            {
+                return new OkObjectResult("Pass a type into the query string");
+            }
+
+            string drafts = req.Query["drafts"];
+            var includeDrafts = Convert.ToBoolean(drafts);
+
+            List<Content> contents = new List<Content>();
+
+            var filter = $"Type eq '{type}'";
+
+            contents = _storageService.QueryContents(filter, includeDrafts, default);
+
+            return new OkObjectResult(contents);
+        }
+    }
+}
