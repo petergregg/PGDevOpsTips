8e8ef99815910ecb8fec0a31869c44aa8eec852b
Peter Gregg <greggp1987@hotmail.com>
2023-12-09 12:39:57 +0000

    Revert "Set fetch-depth to 0 for the checkout action to ensure that the generate-sitemap action has access to the commit history, which is used for generating the <lastmod> tags in the sitemap.xml file."
    This reverts commit deb3d901546bd4dd591f8ee32390e07f7b4946eb.

--- PublishAndPrerender.ps1
+++ PublishAndPrerender.ps1
@@ -8,7 +8,7 @@ Push-Location .\Prerender
 npx react-snap
 Get-ChildItem ".\output\wwwroot\*.html" -Recurse | ForEach-Object { 
     $HtmlFileContent = (Get-Content -Path $_.FullName -Raw);
-    $HtmlFileContent = $HtmlFileContent.Replace('<script>var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;</script><script src="_framework/dotnet.6.0.25.fveiv4nhbd.js" defer="" integrity="sha256-1jUGCCSGH9JWhkqbHdQk8JBjBI8K/VC63En5zlypVps=" crossorigin="anonymous"></script>','')
+    $HtmlFileContent = $HtmlFileContent.Replace('<script>var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;</script><script src="_framework/dotnet.6.0.22.916mi3e43m.js" defer="" integrity="sha256-Vpgzm0BMIfPGXG2SCtEChw5HmXlQJIIS0eyBG8kP+u4=" crossorigin="anonymous"></script>','')
     Set-Content -Path $_.FullName -Value $HtmlFileContent
 }
 Pop-Location
--- PublishAndPrerender.sh
+++ PublishAndPrerender.sh
@@ -3,8 +3,8 @@ rm -rf Prerender/output
 dotnet publish .\/PGDevOpsTips.Web\/PGDevOpsTips.Web.csproj -c Release -o Prerender\/output --nologo
 pushd Prerender
 npx react-snap
-find ./output/wwwroot -name "*.html" | while read htmlFile; do
+find ./output -name "*.html" | while read htmlFile; do
     #sed -i 's/<base href="\/"/<base href="\/PGDevOpsTips\/"/g' $htmlFile
-    sed -i 's/<script>var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;<\/script><script src="_framework\/dotnet.6.0.25.fveiv4nhbd.js" defer="" integrity="sha256-1jUGCCSGH9JWhkqbHdQk8JBjBI8K\/VC63En5zlypVps=" crossorigin="anonymous"><\/script>//g' $htmlFile
+    sed -i 's/<script type="text\/javascript">var Module; window.__wasmmodulecallback__(); delete window.__wasmmodulecallback__;<\/script><script src="_framework\/dotnet.6.0.22.916mi3e43m.js" defer="" integrity="sha256-Vpgzm0BMIfPGXG2SCtEChw5HmXlQJIIS0eyBG8kP+u4=" crossorigin="anonymous"><\/script>//g' $htmlFile
 done
 popd
