72ea2a9cdba457ceeac8656bd5f9aa4593a48217
Peter Gregg <greggp1987@hotmail.com>
2024-01-04 18:13:20 +0000

    Add cookies popup

--- PGDevOpsTips.Web/Program.cs
+++ PGDevOpsTips.Web/Program.cs
@@ -1,3 +1,4 @@
+using BytexDigital.Blazor.Components.CookieConsent;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,52 @@ namespace PGDevOpsTips.Web
             services.AddHttpClient<RestContentService>("contentapi", client =>
             {
                 client.BaseAddress = new Uri(configuration["ContentAPI"]);
+            });
+
+            services.AddCookieConsent(o =>
+            {
+                o.Revision = 1;
+                o.PolicyUrl = "/Cookie-Policy";
+
+                // Call optional
+                o.UseDefaultConsentPrompt(prompt =>
+                {
+                    prompt.Position = ConsentModalPosition.BottomRight;
+                    prompt.Layout = ConsentModalLayout.Bar;
+                    prompt.SecondaryActionOpensSettings = false;
+                    prompt.AcceptAllButtonDisplaysFirst = false;
+                });
+
+                o.Categories.Add(new CookieCategory
+                {
+                    TitleText = new()
+                    {
+                        ["en"] = "Google Services"
+                    },
+                    DescriptionText = new()
+                    {
+                        ["en"] = "Allows the integration and usage of Google services."
+                    },
+                    Identifier = "google",
+                    IsPreselected = true,
+
+                    Services = new()
+                    {
+                        new CookieCategoryService
+                        {
+                            Identifier = "google-analytics",
+                            PolicyUrl = "https://policies.google.com/privacy",
+                            TitleText = new()
+                            {
+                                ["en"] = "Google Analytics"
+                            },
+                            ShowPolicyText = new()
+                            {
+                                ["en"] = "Display policies"
+                            }
+                        }
+                    }
+                });
             });
         }
     }
